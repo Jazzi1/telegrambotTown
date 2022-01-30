c83d9bb9130a6ea609bc1b63494967a7b6dc4fbb
Jazzi1 <kalaev.rus@mail.ru>
2022-01-30 19:20:37 +0300

    1st commit

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext, Filters
+from telegram.ext import Updater, CommandHandler, CallbackContext, Filters, MessageHandler
+
+def start_command(update: Update, _: CallbackContext):
+    update.effective_chat.send_message(text=)
+
+
+def main(token:str):
+    updater = Updater(token=token)
+    dispatcher = updater.dispatcher
+    start_handler = CommandHandler('start', start_command)
+
+
+if __name__ == '__main__':
+    main('5049350468:AAFKVzYmElqUZqO0aDoitV-ELnL4_czafBo')
+
+
+
