e52ba2fb790a69a90334da1f25aede94a958b09f
Jazzi1 <kalaev.rus@mail.ru>
2022-01-31 20:35:02 +0300

    Добавил проверку на мягкий знак

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 
-with open("cities.json", "r") as read_file:
+with open("cities.json", "r", encoding='UTF-8') as read_file:
     data = json.load(read_file)
 
 cities = [i['name'] for i in data['city']]
@@ -16,14 +16,22 @@ def is_exists_town(check_town: str):
             return True
     return False
 
+
 def get_town_by_letter(first_letter: str):
     for town in cities:
         if town not in used_cities and town[0] == first_letter:
             return town
     return None
 
+
+def get_last_letter(town: str):
+    for i in range(len(town) - 1, -1, -1):
+        if town[i] != 'Ъ' and town[i] != 'Ь':
+            return town[i]
+
+
 def start_command(update: Update, _: CallbackContext):
-    update.effective_chat.send_message(text='Здравствуйте, это бот создан для игры в города! Напишите любой город и давайте анчнем игру!')
+    update.effective_chat.send_message(text='Здравствуйте, это бот для игры в города! Напишите город и начнем игру!')
 
 
 def get_town_from_user(update: Update, _: CallbackContext):
@@ -33,18 +41,21 @@ def get_town_from_user(update: Update, _: CallbackContext):
 
     print(last_town_bot, town)
     if town in used_cities:
-        update.effective_chat.send_message(text='Ха,попались! Такой город уже был! Напишите,пожалуйста, новый и мы продолжим игру!.')
+        update.effective_chat.send_message(text='Ха,попались! Такой город уже был! '
+                                                'Напишите новый и мы продолжим игру!.')
     elif not is_exists_town(town):
-        update.effective_chat.send_message(text='Кажется,вы написали несуществующий город. Пожалуйста, напишите новый и мы продолжим игру!')
-    elif last_town_bot == "" or last_town_bot[-1] == town[0]:
-        bot_town = get_town_by_letter(town[-1])
+        update.effective_chat.send_message(text='Кажется,вы написали несуществующий город. Пожалуйста, напишите новый!')
+    elif last_town_bot == "" or get_last_letter(last_town_bot) == town[0]:
+        print(get_last_letter(town))
+        bot_town = get_town_by_letter(get_last_letter(town))
         update.effective_chat.send_message(text=bot_town)
         last_town_bot = bot_town.upper()
 
         used_cities.append(town)
         used_cities.append(bot_town)
     else:
-        update.effective_chat.send_message(text='Извините, такой город не подходит. Напишите новый и мы продолжим игру!')
+        update.effective_chat.send_message(text='Извините, такой город не подходит. '
+                                                'Напишите новый и мы продолжим игру!')
 
 
 def main(token: str):
@@ -60,7 +71,5 @@ def main(token: str):
 
 
 if __name__ == "__main__":
-    main('5049350468:AAFKVzYmElqUZqO0aDoitV-ELnL4_czafBo')
-
-
-
+    with open('token.txt', 'r') as token:
+        main(token.read())
